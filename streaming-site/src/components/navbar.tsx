5a0c60b519ac0377559f0eac7c9164a2aa1dc645
2314577483 <liuzhenjie0619@gmail.com>
2023-08-13 03:17:31 -0700

    new profile page

--- streaming-site/src/components/navbar.tsx
+++ streaming-site/src/components/navbar.tsx
@@ -39,7 +39,7 @@ function Navbar() {
   return (
     <nav className="flex items-center justify-between bg-gray-900 flex-wrap px-6 py-1">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <Link href="/homepage" className="font-semibold text-xl tracking-tight">
+        <Link href="/" className="font-semibold text-xl tracking-tight">
           Fantastic Four
         </Link>
       </div>
@@ -63,7 +63,7 @@ function Navbar() {
       >
         <div className="text-sm lg:flex-grow">
           <Link
-            href="/homepage"
+            href="/"
             className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
           >
             Home
--- streaming-site/src/pages/settings/index.tsx
+++ streaming-site/src/pages/settings/index.tsx
@@ -1,79 +1,109 @@
-/* eslint-disable @next/next/no-img-element */
 import React, { useState } from "react";
 import Navbar from "../../components/navbar";
 import Footer from "../../components/footer";
-import Player from "../../components/player";
 
-import Link from "next/link";
+function Settingpage(): JSX.Element {
+  const [profile, setProfile] = useState({
+    profilePicture: "",
+    username: "",
+    email: "",
+    birthday: "",
+    name: "",
+  });
 
-function Homepage(): JSX.Element {
-  const categories = ["Following", "VALORANT", "League of Legends"];
-  const streams = [1, 2, 3];
-  const videos = [
-    "https://34.83.97.105/streams/spacex/index.m3u8",
-    "https://34.83.97.105/streams/obs/index.m3u8",
-    "https://34.83.97.105/streams/mobile/index.m3u8",
-  ];
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const nextVideo = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % videos.length);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setProfile({
+      ...profile,
+      [name]: value,
+    });
   };
-
-  const prevVideo = () => {
-    setCurrentIndex(
-      (prevIndex) => (prevIndex - 1 + videos.length) % videos.length
-    );
-  };
-
 
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
       <Navbar></Navbar>
 
       <main className="flex-1">
-        <div className="container mx-auto p-8">
-          <h1 className="text-3xl font-bold my-4">Settings</h1>
-          <hr className="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700"/>
+        <div className="container mx-auto px-8 py-6">
+          <h1 className="text-3xl font-bold mb-4">Settings</h1>
+          <hr className="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700" />
+
+          <section className="mb-8">
+            <h2 className="text-xl font-bold mb-4">Profile Picture</h2>
+            <div className="rounded overflow-hidden shadow-lg p-4 bg-white relative flex items-center">
+              <div className="flex items-center">
+                <img
+                  src={
+                    profile.profilePicture ||
+                    "path/to/default/profile/picture.png"
+                  }
+                  alt="Profile"
+                  className="w-16 h-16 rounded-full"
+                />
+                <input
+                  type="file"
+                  name="profilePicture"
+                  className="ml-4"
+                  onChange={(e) =>
+                    setProfile({
+                      ...profile,
+                      profilePicture: URL.createObjectURL(e.target.files?.[0]),
+                    })
+                  }
+                />
+                <button className="bg-blue-500 text-white px-4 py-2 rounded ml-4">Add Profile Picture</button>
+              </div>
+            </div>
+          </section>
 
           <section>
-            <h2 className="text-xl font-bold my-4">Profile Picture</h2>
-              <div className="justify-between flex rounded overflow-hidden shadow-lg p-4 bg-white relative">
-                <div className="flex">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    className="w-16 h-16"
-                  >
-                  <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+            <h2 className="text-xl font-bold mb-4">Profile Settings</h2>
+            <div className="rounded overflow-hidden shadow-lg p-4 bg-white relative">
+              <div className="grid grid-cols-2 gap-4 w-full">
+                <div className="col-span-1">
+                  <label htmlFor="username" className="text-gray-700">Username</label>
+                  <input
+                    type="text"
+                    name="username"
+                    value={profile.username}
+                    onChange={handleChange}
+                    className="w-full p-2 mt-1 rounded border"
                   />
-                  </svg>
-
-                  <div>
-                    <button>Add Profile Picture</button>
-                  </div>
+                </div>
+                <div className="col-span-1">
+                  <label htmlFor="email" className="text-gray-700">Email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    value={profile.email}
+                    onChange={handleChange}
+                    className="w-full p-2 mt-1 rounded border"
+                  />
+                </div>
+                <div className="col-span-1">
+                  <label htmlFor="birthday" className="text-gray-700">Birthday</label>
+                  <input
+                    type="date"
+                    name="birthday"
+                    value={profile.birthday}
+                    onChange={handleChange}
+                    className="w-full p-2 mt-1 rounded border"
+                  />
+                </div>
+                <div className="col-span-1">
+                  <label htmlFor="name" className="text-gray-700">Name</label>
+                  <input
+                    type="text"
+                    name="name"
+                    value={profile.name}
+                    onChange={handleChange}
+                    className="w-full p-2 mt-1 rounded border"
+                  />
                 </div>
               </div>
-            </section>
-
-            <section>
-              <h2 className="text-xl font-bold my-4">Profile Settings</h2>
-                <div className="flex rounded overflow-hidden shadow-lg p-4 bg-white relative">
-                  <div className="grid grid-rows-3 grid-flow-col">
-                    <div className="col-span-2">02</div>
-                    <div className="col-span-2">01</div>
-
-
-                  </div>
-                </div>
-            </section>
-          </div>
+            </div>
+          </section>
+        </div>
       </main>
 
       <Footer></Footer>
@@ -81,4 +111,4 @@ function Homepage(): JSX.Element {
   );
 }
 
-export default Homepage;
+export default Settingpage;
