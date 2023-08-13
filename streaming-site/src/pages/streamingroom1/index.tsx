556ec985f4376de38d46d8f2115c8f5201fcbb4b
Aryan-Obrai <hobra001@ucr.edu>
2023-08-12 19:44:41 -0700

    WIP

--- streaming-site/src/pages/streamingroom1/index.tsx
+++ streaming-site/src/pages/streamingroom1/index.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useRef } from "react";
 import Navbar from "../../components/navbar";
 import Footer from "../../components/footer";
 import Player from "../../components/player";
+import Link from "next/link";
 
 function StreamingRoom(): JSX.Element {
   const [messages, setMessages] = useState<{ text: string; timestamp: Date }[]>(
@@ -93,6 +94,24 @@ function StreamingRoom(): JSX.Element {
                       </svg>
                       Share
                     </button>
+
+                    <Link href="/checkout" className="text-center ml-1 bg-gray-900 text-white font-bold rounded-lg px-2 py-1 hover:bg-gray-600">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        className="w-6 h-6 inline-block mr-1"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
+                        />
+                      </svg>
+                      Donate
+                    </Link>
                   </div>
                 </div>
 
