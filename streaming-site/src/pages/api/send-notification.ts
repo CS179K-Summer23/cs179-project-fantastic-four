c25306497c479af9a6d5816ff46a3216d2f9a1f1
Jaired Jawed <jairedjawed@icloud.com>
2023-08-21 14:38:40 -0700

    send stream notifications when a user goes live

---
+++ streaming-site/src/pages/api/send-notification.ts
@@ -0,0 +1,37 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const endpoint = async (req: NextApiRequest, res: NextApiResponse) => {
+  const sgMail = require('@sendgrid/mail')
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+  req.body.users.forEach(async (user: any) => {
+    if (!user.email) return
+
+    if (user.email === req.body.streamer.email) {
+      return
+    }
+
+    const msg = {
+      to: user.email,
+      from: 'mail@fantasticfourstreaming.herokuapp.com',
+      subject: 'Streamer ' + req.body.streamer.name + ' is live!',
+      text: 'A streamer from our site is named ' + req.body.streamer.name + ' is live! Come check them out!',
+      html: '<strong>New episode of Fantastic Four</strong>',
+    }
+
+    console.log(msg)
+
+    try {
+      // await sgMail.send(msg);
+      console.log('Message', msg)
+    }
+    catch (error) {
+      console.log('ERROR', error);
+    }
+  })
+
+  res.status(200).json({ message: 'Message sent successfully.' })
+
+}
+
+export default endpoint
--- streaming-site/src/pages/streams/index.tsx
+++ streaming-site/src/pages/streams/index.tsx
@@ -31,7 +31,7 @@ function Streams(): JSX.Element {
     name: string;
     avatar_url: string;
   }
-  
+
   const [streams, setStreams] = useState<Stream[]>([]);
   const [streamers, setStreamers] = useState<Streamer[]>([]);
 
@@ -50,35 +50,35 @@ function Streams(): JSX.Element {
 
     const fetchStreamer = async (streamer_id: string) => {
       const streamerQuery = query(
-        usersRef, 
+        usersRef,
         where("id", "==", streamer_id)
       );
 
       const userSnapshot = await getDocs(streamerQuery);
       return userSnapshot.docs[0].data();
-      };
-    
+    };
+
     const fetchStreams = async () => {
       const streamsQuery = query(
         collection(db, 'streams'),
         orderBy('view_count', 'desc')
-      ); 
+      );
       const streamsSnapshot = await getDocs(streamsQuery);
 
       const streamsArr: any[] = await Promise.all(streamsSnapshot.docs
-          .filter((stream) => stream.data()['end_time'] === null)
-          .map( async (stream) => {   
-                      return stream.data();
-      }));
+        .filter((stream) => stream.data()['end_time'] === null)
+        .map(async (stream) => {
+          return stream.data();
+        }));
 
       const streamersArr: any[] = await Promise.all(streamsArr
-        .map( async(stream) => {
+        .map(async (stream) => {
           return await fetchStreamer(stream['streamer_id'])
             .then(async (data) => {
               console.log('data', data);
               return data;
-          });
-      }));
+            });
+        }));
 
       console.log('a', streamsArr);
       console.log('b', streamersArr);
@@ -96,99 +96,157 @@ function Streams(): JSX.Element {
     }).catch(error => {
       console.log(error);
     })
-    
+
   }, []);
+
+  useEffect(() => {
+    let streamers: any = null;
+    let streamerIds: any[] = [];
+
+    const userRetrieval = setInterval(async function () {
+      /* retrieve users from firestore every 5 seconds */
+      const { auth, db } = getFirebaseApp();
+      if (!auth || !db) {
+        console.error("Firebase not available");
+        return;
+      }
+
+      const usersRef = getDocs(collection(db, "users"));
+      const usersSnapshot = await usersRef;
+      if (streamers === null) {
+        streamers = usersSnapshot.docs.map((doc) => doc.data()).filter((user) => {
+          if (user['streaming_status'] === true) {
+            streamerIds.push(user['id']);
+            return true
+          }
+
+          return false
+        });
+      }
+      else {
+      streamers.forEach(async (streamer: any) => {
+        try {
+          await fetch('/api/send-notification', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              streamer: streamer,
+              users: usersSnapshot.docs.map((doc) => ({
+                ...doc.data(),
+                // email: 'jairedjawed@icloud.com'
+              }))
+            })
+          })
+        }
+        catch (error) {
+          console.log(error);
+        }
+      })
+
+
+      // filter streamers that are different from the previous snapshot that are streaming
+      streamers = usersSnapshot.docs.map((doc) => doc.data()).filter((user) => user['streaming_status'] === true && !streamerIds.includes(user['id']))
+    }
+
+    }, 1000);
+
+    return () => {
+      clearTimeout(userRetrieval);
+    }
+  }, [])
 
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
       <Navbar></Navbar>
       <main className="flex-1">
         <div className="container mx-auto p-8">
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
-                {streams.map((stream, i) => (
-                  <div
-                    key={stream.id}
-                    className="rounded overflow-hidden shadow-lg p-4 bg-white"
-                  >
-                    <div className="relative pb-3/2">
-                      <Player
-                        autoplay
-                          muted
-                          preload="auto"
-                          src={stream.stream_url}
-                          poster={stream.thumbnail_url}
-                        />  
-                    </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+            {streams.map((stream, i) => (
+              <div
+                key={stream.id}
+                className="rounded overflow-hidden shadow-lg p-4 bg-white"
+              >
+                <div className="relative pb-3/2">
+                  <Player
+                    autoplay
+                    muted
+                    preload="auto"
+                    src={stream.stream_url}
+                    poster={stream.thumbnail_url}
+                  />
+                </div>
 
-                    <div className="">
-                      <Link href={'/streamingroom/' + streamers[i].name}>
-                        <div
-                          className="relative"
-                        >
-                          <iframe
-                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/1280px-A_black_image.jpg"
-                            className="absolute top-0 left-0 w-full h-full z-8"
-                            frameBorder="0"
-                            scrolling="no"
-                            allowFullScreen={true}
-                          ></iframe>
-                          <div
-                            style={{
-                              position: "absolute",
-                              top: 0,
-                              left: 0,
-                              width: "100%",
-                              height: "100%",
-                              zIndex: 2,
-                            }}
-                          ></div>
-                        </div>
-                      </Link>
-
-                      <div>
-                        <img
-                          src="https://cdn-icons-png.flaticon.com/512/149/149071.png?w=740&t=st=1691147917~exp=1691148517~hmac=eb6166a62265ce27b7afac68d87a03b748bc37c5361e49e55c8ced8a2f60e2db"
-                          className="mt-2 w-7 h-7 rounded-full float-left mr-2"
-                          alt="Streamer avatar"
-                        />
-                        <div className="pl-9">
-                          <h3 className="font-bold text-xl hover:text-gray-500">
-                            <Link href={'/streamingroom/' + streamers[i].name}>
-                              {stream.title}
-                            </Link>
-                          </h3>
-                          <div className="flex justify-between items-center">
-
-                          <Link
-                            href={'/streamingroom/' + streamers[i].name}
-                            className="text-sm text-gray-700 hover:text-gray-500"
-                          >
-                            {streamers[i].name}
-                          </Link>
-                          <span className="flex text-gray-600 text-m pr-6 pt-2">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      className="w-6 h-6"
+                <div className="">
+                  <Link href={'/streamingroom/' + streamers[i].name}>
+                    <div
+                      className="relative"
                     >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"
-                      />
-                    </svg>
-                    {stream?.view_count}
-                  </span>
-                        </div>
-                        </div>
+                      <iframe
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/1280px-A_black_image.jpg"
+                        className="absolute top-0 left-0 w-full h-full z-8"
+                        frameBorder="0"
+                        scrolling="no"
+                        allowFullScreen={true}
+                      ></iframe>
+                      <div
+                        style={{
+                          position: "absolute",
+                          top: 0,
+                          left: 0,
+                          width: "100%",
+                          height: "100%",
+                          zIndex: 2,
+                        }}
+                      ></div>
+                    </div>
+                  </Link>
+
+                  <div>
+                    <img
+                      src="https://cdn-icons-png.flaticon.com/512/149/149071.png?w=740&t=st=1691147917~exp=1691148517~hmac=eb6166a62265ce27b7afac68d87a03b748bc37c5361e49e55c8ced8a2f60e2db"
+                      className="mt-2 w-7 h-7 rounded-full float-left mr-2"
+                      alt="Streamer avatar"
+                    />
+                    <div className="pl-9">
+                      <h3 className="font-bold text-xl hover:text-gray-500">
+                        <Link href={'/streamingroom/' + streamers[i].name}>
+                          {stream.title}
+                        </Link>
+                      </h3>
+                      <div className="flex justify-between items-center">
+
+                        <Link
+                          href={'/streamingroom/' + streamers[i].name}
+                          className="text-sm text-gray-700 hover:text-gray-500"
+                        >
+                          {streamers[i].name}
+                        </Link>
+                        <span className="flex text-gray-600 text-m pr-6 pt-2">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"
+                            />
+                          </svg>
+                          {stream?.view_count}
+                        </span>
                       </div>
                     </div>
                   </div>
-                ))}
-              </div> 
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </main>
       <Footer></Footer>
