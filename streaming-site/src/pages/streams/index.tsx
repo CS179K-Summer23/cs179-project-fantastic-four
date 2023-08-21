bb552411b4d191e67ae1bbdd643be80a1fc52903
notpallav <43105309+notpallav@users.noreply.github.com>
2023-08-21 13:45:40 -0700

    sort streams by desc view count

--- streaming-site/src/pages/streams/index.tsx
+++ streaming-site/src/pages/streams/index.tsx
@@ -7,7 +7,7 @@ import { string, bool, number } from 'prop-types';
 
 import Link from "next/link";
 
-import { QuerySnapshot, collection, doc, getDocs, getDoc, query, where, orderBy } from "firebase/firestore";
+import { QuerySnapshot, collection, doc, getDocs, getDoc, query, where, orderBy, QueryOrderByConstraint } from "firebase/firestore";
 import { getFirebaseApp } from "../../utils/firebase.config";
 
 
@@ -61,7 +61,7 @@ function Streams(): JSX.Element {
     const fetchStreams = async () => {
       const streamsQuery = query(
         collection(db, 'streams'),
-        orderBy('view_count')
+        orderBy('view_count', 'desc')
       ); 
       const streamsSnapshot = await getDocs(streamsQuery);
 
@@ -157,12 +157,32 @@ function Streams(): JSX.Element {
                               {stream.title}
                             </Link>
                           </h3>
+                          <div className="flex justify-between items-center">
+
                           <Link
                             href={'/streamingroom/' + streamers[i].name}
                             className="text-sm text-gray-700 hover:text-gray-500"
                           >
                             {streamers[i].name}
                           </Link>
+                          <span className="flex text-gray-600 text-m pr-6 pt-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"
+                      />
+                    </svg>
+                    {stream?.view_count}
+                  </span>
+                        </div>
                         </div>
                       </div>
                     </div>
