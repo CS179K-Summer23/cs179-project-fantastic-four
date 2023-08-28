a00dc62b47e2666f341a2993d1f9c4ffb06e4f01
Jaired Jawed <jairedjawed@icloud.com>
2023-08-28 16:45:02 -0700

    updated following page to list streams that user follows

--- streaming-site/src/pages/following/index.tsx
+++ streaming-site/src/pages/following/index.tsx
@@ -1,13 +1,70 @@
-/* eslint-disable @next/next/no-img-element */
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../../components/navbar";
 import Footer from "../../components/footer";
 
+import Image from "next/image";
 import Link from "next/link";
+
+import { onAuthStateChanged } from 'firebase/auth'
+import { collection, onSnapshot, query, where } from 'firebase/firestore'
+import { getFirebaseApp } from '../../utils/firebase.config'
+
+type Stream = {
+  id: string
+  title: string
+  description: string
+}
 
 function Followingpage(): JSX.Element {
   const categories = ["Following"];
-  const streams = [1, 2, 3];
+
+  const [streamsLoading, setStreamLoadingStatus] = useState<boolean>(true);
+  const [streams, setStreams] = useState<Stream[]>([]);
+
+  useEffect(() => {
+    const { db, auth } = getFirebaseApp()
+
+    if (!db || !auth) return;
+
+    let unsubscribeFollowing: any = null;
+    let unsubscribeStreams: any = null;
+
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // get following list
+        const followingQuery = query(collection(db, 'follows'), where('followerId', '==', user.uid))
+
+        unsubscribeFollowing = onSnapshot(followingQuery, (followingSnapshot) => {
+          const following = followingSnapshot.docs.map((doc) => doc.data().followingId)
+
+          // get streams from following list
+          const streamersQuery = query(collection(db, 'streams'), where('streamer_id', 'in', following))
+
+          unsubscribeStreams = onSnapshot(streamersQuery, (streamsSnapshot) => {
+            const streams = streamsSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() } as Stream))
+            setStreams(streams)
+          })
+        })
+
+        setStreamLoadingStatus(false)
+      }
+      else {
+        setStreamLoadingStatus(false)
+      }
+    })
+
+    return () => {
+      if (unsubscribeFollowing !== null) {
+        unsubscribeFollowing()
+      }
+
+      if (unsubscribeStreams !== null) {
+        unsubscribeStreams()
+      }
+    };
+  }, [])
+
+  console.log('streams', streams)
 
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
@@ -19,9 +76,9 @@ function Followingpage(): JSX.Element {
             <section key={category}>
               <h2 className="text-2xl font-bold my-4">{category}</h2>
               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
-                {streams.map((stream) => (
+                {!streamsLoading && streams.length > 0 && streams.map((stream) => (
                   <div
-                    key={stream}
+                    key={stream.id}
                     className="rounded overflow-hidden shadow-lg p-4 bg-white"
                   >
                     <div className="relative pb-3/2">
@@ -51,7 +108,7 @@ function Followingpage(): JSX.Element {
                             frameBorder="0"
                             scrolling="no"
                             allowFullScreen={true}
-                          ></iframe>
+                          />
                           <div
                             style={{
                               position: "absolute",
@@ -66,22 +123,24 @@ function Followingpage(): JSX.Element {
                       </Link>
 
                       <div>
-                        <img
+                        <Image
                           src="https://cdn-icons-png.flaticon.com/512/149/149071.png?w=740&t=st=1691147917~exp=1691148517~hmac=eb6166a62265ce27b7afac68d87a03b748bc37c5361e49e55c8ced8a2f60e2db"
                           className="mt-2 w-7 h-7 rounded-full float-left mr-2"
                           alt="Streamer avatar"
+                          width={100}
+                          height={100}
                         />
                         <div className="pl-9">
                           <h3 className="font-bold text-xl hover:text-gray-500">
                             <Link href="/streamingroom">
-                              Stream Title {stream}
+                              Stream Title {stream.title}
                             </Link>
                           </h3>
                           <Link
                             href="/streamingroom"
                             className="text-sm text-gray-700 hover:text-gray-500"
                           >
-                            Stream description here...
+                            {stream.description}
                           </Link>
                         </div>
                       </div>
@@ -94,7 +153,7 @@ function Followingpage(): JSX.Element {
         </div>
       </main>
 
-      <Footer></Footer>
+      <Footer />
     </div>
   );
 }
