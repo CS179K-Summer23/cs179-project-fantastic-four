26dedd979325aa0f09b75f685dc8aeb65c79d13e
Jaired Jawed <jairedjawed@icloud.com>
2023-08-28 15:11:58 -0700

    changed color for follow/following status button

--- streaming-site/src/pages/[stream].tsx
+++ streaming-site/src/pages/[stream].tsx
@@ -194,9 +194,7 @@ function StreamingRoom(): JSX.Element {
                   <h2 className="font-bold text-xl mb-2">{stream?.title || streamer?.title}</h2>
                   <div className="text-gray-600 text-m pt-2">
                   <button
-                      className={`text-center ${
-                       followListLoading ? "bg-gray-900" : isFollowed ? "bg-red-600" : "bg-gray-900"
-                      } text-white font-bold rounded-lg px-2 py-1 hover:bg-gray-600`}
+                      className={`text-cente text-white font-bold rounded-lg px-2 py-1 ${followListLoading ? "bg-gray-600" : isFollowed ? "bg-gray-900" : "bg-gray-800"} hover:bg-gray-600`}
                       onClick={handleFollow}
                     >
                       <svg
@@ -213,7 +211,7 @@ function StreamingRoom(): JSX.Element {
                           d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
                         />
                       </svg>
-                      {followListLoading ? "Loading..." : isFollowed ? "Unfollow" : "Follow"}
+                      {followListLoading ? "Loading..." : isFollowed ? "Following" : "Follow"}
                     </button>
 
                     <button className="text-center ml-1 bg-gray-900 text-white font-bold rounded-lg px-2 py-1 hover:bg-gray-600">
