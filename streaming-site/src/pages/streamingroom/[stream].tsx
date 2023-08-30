029d380394e3495ca488dfe818b039d3057d3dd2
Aryan-Obrai <hobra001@ucr.edu>
2023-08-30 13:16:09 -0700

    Changed payment link for updated logs

--- streaming-site/src/pages/streamingroom/[stream].tsx
+++ streaming-site/src/pages/streamingroom/[stream].tsx
@@ -182,25 +182,15 @@ function StreamingRoom(): JSX.Element {
                       </svg>
                       Share
                     </button>
-                    <button
-                      onClick={async () => {
-                          await checkout({
-                              mode: 'payment',
-                              lineItems: [
-                                  {
-                                      price: "price_1NiQ1EEqkNVKC3nW2zGK7Rh3",
-                                      quantity: 1
-                                  }
-                              ],
-                          });
-                      }}
-                      className="text-center ml-1 bg-gray-900 text-white font-bold rounded-lg px-2 py-1 hover:bg-gray-600"
+                    <a
+                      href="https://buy.stripe.com/test_00gg1R5O83QoaKk6op"
+                      className="text-center ml-1 bg-gray-900 text-white font-bold rounded-lg px-2 py-1.5 hover:bg-gray-600"
                   >
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 inline-block mr-1">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 25 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 inline-block mr-1">
                           <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                       </svg>
                       Donate
-                  </button>
+                  </a>
                   <button
                       onClick={async () => {
                           await checkout({
