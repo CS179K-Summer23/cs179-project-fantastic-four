f78a27b5899d600ca3644a2ecdfd13fbbe4b6979
notpallav <43105309+notpallav@users.noreply.github.com>
2023-08-30 15:41:18 -0700

    more

--- streaming-site/src/pages/[stream].tsx
+++ streaming-site/src/pages/[stream].tsx
@@ -10,6 +10,8 @@ import DonationForm from "../components/donation-form";
 import Chat from '../components/chat';
 import Bans from "../components/bans";
 import Mods from "../components/mods";
+import Share from "../components/share";
+import Link from "next/link";
 
 import { Unsubscribe, onAuthStateChanged } from "firebase/auth";
 import { getFirebaseApp } from "../utils/firebase.config";
@@ -460,23 +462,7 @@ const handleBan = async () => {
                     </button>)}
 
                     {!streamerLoading && streamer &&
-                    !isStreamerBanned && (<button className="text-center ml-1 bg-gray-900 text-white font-bold rounded-lg px-2 py-1 hover:bg-gray-600">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        className="w-6 h-6 inline-block mr-1"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
-                        />
-                      </svg>
-                      Share
-                    </button>)}
+                    !isStreamerBanned && (<Share></Share>)}
                     {!isLoadingUser && user && 
                      !streamerLoading && streamer &&
                      !isStreamerBanned &&
@@ -513,8 +499,13 @@ const handleBan = async () => {
                 </div>
 
                 <div className="flex justify-between items-center pl-9">
-                  <p className="text-gray-700 text-base">
+                  <p className="text-gray-700">
                     {streamer?.name}
+                    <Link href={"/categories/" + stream?.category}>
+                      <span className="ml-3 hover:bg-gray-400 rounded-lg text-xs p-1 font-bold bg-gray-300">
+                        {stream?.category}
+                      </span>
+                    </Link>
                   </p>
                   {stream && (
                   <span className="flex text-gray-600 text-m pr-6 pt-2">
